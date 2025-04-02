696f29b639892ccfdb7dec66899ca21f32f1807d
GiantRat985 <156049816+GiantRat985@users.noreply.github.com>
2025-04-01 21:24:19 -1000

    chore: add appsettings file and integrate into framework

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration.Json;
+
+using System.Reflection;
 
 namespace MovieRepoApp
 {
@@ -15,11 +19,37 @@ namespace MovieRepoApp
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            builder.AddAppSettings();
+
+            // Reads settings configurations
+            string? omdbApiKey = builder.Configuration.GetValue<string>("OmdbApiKey");
+            string? omdbDataEndpoint = builder.Configuration.GetValue<string>("OmdbDataEndpoint");
+            string? omdbPosterEndpoint = builder.Configuration.GetValue<string>("OmdbPosterEndpoint");
+
 #if DEBUG
     		builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
+        }
+
+        /// <summary>
+        /// Reads settings from settings file.
+        /// </summary>
+        /// <param name="builder"></param>
+        private static void AddAppSettings(this MauiAppBuilder builder)
+        {
+            using Stream? stream = Assembly
+                .GetExecutingAssembly()
+                .GetManifestResourceStream("MovieRepoApp.appsettings.json");
+
+            if (stream != null)
+            {
+                IConfigurationRoot config = new ConfigurationBuilder()
+                    .AddJsonStream(stream)
+                    .Build();
+                builder.Configuration.AddConfiguration(config);
+            }
         }
     }
 }
