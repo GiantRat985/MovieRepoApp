2e628e070a8b9d1e5d68ef44c8950d2c6e486222
GiantRat985 <156049816+GiantRat985@users.noreply.github.com>
2025-04-07 18:19:34 -1000

    chore: add infrastructure classes
    Add infrastructure classes.

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRepoApp.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Event member for the <see cref="INotifyPropertyChanged"/> interface
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+        /// <summary>
+        /// Event invocation for the <see cref="INotifyPropertyChanged"/> interface
+        /// </summary>
+        /// <param name="name"></param>
+        protected void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
