aa5656890027321a8f0dc3a69d2331815de63d3f
GiantRat985 <156049816+GiantRat985@users.noreply.github.com>
2025-04-07 18:19:34 -1000

    chore: add infrastructure classes
    Add infrastructure classes.

---
+++ ViewModels/Command Helpers/AsyncCommand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRepoApp.ViewModels
+{
+    public class AsyncCommand : IAsyncCommand
+    {
+        private readonly Func<object?, Task> _execute;
+        private readonly Func<object?, bool> _canExecute;
+        private bool _isExecuting;
+
+        public AsyncCommand(Func<object?, Task> execute, Func<object?, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return !_isExecuting && (_canExecute?.Invoke(parameter) ?? true);
+        }
+
+        public async void Execute(object? parameter)
+        {
+            await ExecuteAsync(parameter);
+        }
+
+        public async Task ExecuteAsync(object? parameter)
+        {
+            if (CanExecute(parameter))
+            {
+                try
+                {
+                    _isExecuting = true;
+                    RaiseCanExecuteChanged();
+                    await _execute(parameter);
+                }
+                finally
+                {
+                    _isExecuting = false;
+                    RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+        public virtual void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
