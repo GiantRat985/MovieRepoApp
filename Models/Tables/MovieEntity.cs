369fc691c69cc358e5b25908225be8100b18b400
GiantRat985 <156049816+GiantRat985@users.noreply.github.com>
2025-04-21 21:17:33 -1000

    refactor: refactor models to align with database tables
    Refactor model files to better align with database columns.

---
+++ Models/Tables/MovieEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace MovieRepoApp.Models.Tables
+{
+    public class MovieEntity
+    {
+        [PrimaryKey]
+        public string ImdbID { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Rated { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public string Writer { get; set; }
+        public string Actors { get; set; }
+        public string Plot { get; set; }
+        public string Country { get; set; }
+        public string Poster { get; set; }
+        public string ImdbRating { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ Models/Tables/MovieUserData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace MovieRepoApp.Models.Tables
+{
+    /// <summary>
+    /// Holds user provided data about a specific movie.
+    /// </summary>
+    public class MovieUserData
+    {
+        [PrimaryKey]
+        public string Id { get; set; }
+        [SQLiteNetExtensions.Attributes.ForeignKey(typeof(MovieEntity))]
+        public string ImdbId { get; set; }
+        public string FirstWatchDate { get; set; }
+        public string LastWatchDate {  get; set; }
+        public int Rating { get; set; }
+        public bool Favorite { get; set; }
+        public int WatchCounter { get; set; }
+    }
+}
