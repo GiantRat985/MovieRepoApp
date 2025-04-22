dc6d70980c79be2b24ace6dfc522f21551817899
GiantRat985 <156049816+GiantRat985@users.noreply.github.com>
2025-04-21 21:14:43 -1000

    feat: add a service to handle dialogue box popups
    Add an interface to allow decoupled dialogue box popups from viewmodels.

---
+++ Services/DialogueService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRepoApp.Services
+{
+    public class DialogueService : IDialogueService
+    {
+        public async Task ShowErrorAsync(string message)
+        {
+            await Application.Current.MainPage.DisplayAlert("Error", message, "OK");
+        }
+
+        public async Task ShowMessageAsync(string message)
+        {
+            await Application.Current.MainPage.DisplayAlert("", message, "OK");
+        }
+    }
+}
---
+++ Services/IDialogueService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRepoApp.Services
+{
+    public interface IDialogueService
+    {
+        Task ShowMessageAsync(string message);
+        Task ShowErrorAsync(string message);
+    }
+}
